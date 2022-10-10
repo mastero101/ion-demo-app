4ead87220d478eaaf7497df8b659995825ffb6e2
mastero <castro.alejandro17@gmail.com>
2022-10-10 07:17:46 -0500

    1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'ionic-demo-app-7e2df',
+    appId: '1:723001954026:web:21522cb68d1a9ed0a05193',
+    storageBucket: 'ionic-demo-app-7e2df.appspot.com',
+    apiKey: 'AIzaSyCTvpMZAKislqdcGHBhlRjHIQ0WS3sEh2g',
+    authDomain: 'ionic-demo-app-7e2df.firebaseapp.com',
+    messagingSenderId: '723001954026',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ionic-demo-app-7e2df',
+    appId: '1:723001954026:web:21522cb68d1a9ed0a05193',
+    storageBucket: 'ionic-demo-app-7e2df.appspot.com',
+    apiKey: 'AIzaSyCTvpMZAKislqdcGHBhlRjHIQ0WS3sEh2g',
+    authDomain: 'ionic-demo-app-7e2df.firebaseapp.com',
+    messagingSenderId: '723001954026',
+  },
   production: false
 };
 
